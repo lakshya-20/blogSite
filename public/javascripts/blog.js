d6c8899cc0a79b28cc8bd3f3bf5159afc0fdcb22
Lakshya Bansal <bansallakshya94128@gmail.com>
2020-04-04 23:17:55 +0530

    Changes in profile header

--- public/javascripts/blog.js
+++ public/javascripts/blog.js
@@ -1,14 +1,15 @@
 var modal;
 var btn;
 var span;
+//var blog = document.getElementsByTagName("body");
 function myFunction(title){
   modal = document.getElementById(title);
   btn = document.getElementById("myBtn");
   span = document.getElementsByClassName("close")[0];
   title = document.getElementsByClassName("blog-title");
-    
-  modal.style.display = "block";
   
+  modal.style.display = "block";
+  //blog.style.overflow="hidden";
 }
 function myFunction2() {
   modal.style.display = "none";
