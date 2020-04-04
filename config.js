6193c60cb4d89a4c865a3c38e78eeb65e45ef64f
Lakshya Bansal <bansallakshya94128@gmail.com>
2020-04-04 23:28:00 +0530

    deployment commit

---
+++ config.js
@@ -0,0 +1,4 @@
+const dburl='mongodb+srv://admin:admin@cluster0-qfffq.mongodb.net/test?retryWrites=true&w=majority';
+module.exports={
+    'dburl':"mongodb+srv://admin:admin@cluster0-qfffq.mongodb.net/test?retryWrites=true&w=majority"
+}
