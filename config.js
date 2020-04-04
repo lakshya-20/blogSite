e138012f0b8a887cad675edfcf25b55fb4fd11f6
Lakshya Bansal <39180629+prog-hammer@users.noreply.github.com>
2020-04-04 23:21:45 +0530

    Deleted config file from repo

--- config.js
+++
@@ -1,4 +0,0 @@
-const dburl='mongodb+srv://admin:admin@cluster0-qfffq.mongodb.net/test?retryWrites=true&w=majority';
-module.exports={
-    'dburl':"mongodb+srv://admin:admin@cluster0-qfffq.mongodb.net/test?retryWrites=true&w=majority"
-}
